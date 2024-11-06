50f41cf0a3a366e5d2f25c7a9557e34c9c436128
Divyanshgupta030 <145568562+Divyanshgupta030@users.noreply.github.com>
2024-11-06 21:49:43 +0530

    added test to api add prisma with supabase db

--- apps/api/app/api/[...route]/route.ts
+++ apps/api/app/api/[...route]/route.ts
@@ -1,14 +1,61 @@
+import { prisma } from "@repo/db";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
-export const runtime = "edge";
 
-const app = new Hono().basePath("/api");
+export const runtime = "nodejs";
 
-app.get("/hello", (c) => {
+export const app = new Hono().basePath("/api");
+
+app.get("/hello",async(c) => {
+  const test = await prisma.user.findMany()
   return c.json({
-    message: "Hello from Hono!",
+    test
+  });
+})
+.patch( async(c) => {
+  const name = await c.req.json()
+  const test = await prisma.user.update({
+    where: {
+      id: "123",
+    },
+    data: {
+      name:name.name
+    }
+  })
+  return c.json({
+    test
+  });
+})
+.delete( async(c) => {
+  const test = await prisma.user.delete({
+    where: {
+      id: "2",
+    }
+  })
+  return c.json({
+    test
+  });
+})
+.post(async(c) => {
+  const body =  await c.req.json()
+  console.log(body)
+  const test = await prisma.user.create({
+    data: body
+  })
+  return c.json({
+    test
+  });
+});
+
+app.get("/health", async(c) => {
+  return c.json({
+    message:"i am alive",
+    status:200
   });
 });
 
 export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
