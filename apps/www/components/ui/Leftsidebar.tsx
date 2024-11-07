079e21f03fe4ce3fbc879e4682646b74b55d3285
ritik <ritikkharya05@gmail.com>
2024-11-07 17:35:57 +0530

    usage page updated(2)

--- apps/www/components/ui/Leftsidebar.tsx
+++ apps/www/components/ui/Leftsidebar.tsx
@@ -1,3 +1,6 @@
+import { SiteFooter } from "../custom/site/footer";
+import { ScrollArea } from "./scroll-area";
+
 export default function LeftSidebar() {
   const items = [
     { label: "Introduction" },
@@ -14,25 +17,32 @@ export default function LeftSidebar() {
     { label: "FAQ" },
     { label: "Blog" },
     { label: "Contact Us" },
+    { label: "Pricing" },
+    { label: "FAQ" },
+    { label: "Blog" },
+    { label: "Contact Us" },
   ];
 
   return (
-    <div className="flex flex-col h-screen w-full overflow-y-auto border-r border-border font-semibold text-sm">
-      <div className="flex flex-col ml-10">
-        <span className="text-white h-12 w-full text-base flex items-center">
-          Getting Started
-        </span>
-        <div className="w-full h-full">
-          {items.map((item, index) => (
-            <div
-              key={`${item.label}-${index}`}
-              className="text-white w-full h-10 flex items-center hover:opacity-90 cursor-pointer "
-            >
-              {item.label}
-            </div>
-          ))}
+    <div className="flex flex-col h-full fixed left-0 top-8 w-[16rem] border-r border-border font-semibold text-sm p-4 ">
+      <ScrollArea className="w-[16rem]">
+        <div className="flex flex-col ml-4 w-full">
+          <h1 className="text-white h-12 w-full text-base flex items-center">
+            Getting Started
+          </h1>
+          <div className="w-full h-full">
+            {items.map((item, index) => (
+              <div
+                key={`${item.label}-${index}`}
+                className="text-white w-full h-10 flex items-center hover:opacity-90 cursor-pointer "
+              >
+                {item.label}
+              </div>
+            ))}
+          </div>
+          <SiteFooter />
         </div>
-      </div>
+      </ScrollArea>
     </div>
   );
 }
