bc9d769e2fb5b96c2f20505406b461ffef223b6d
Saidev Dhal <connect.saidev@gmail.com>
2024-11-08 11:47:34 +0530

    add settings page UI

---
+++ apps/app/components/custom/settings/billing.settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionLabel from '../section/section.label'
+
+export default function BillingSettings() {
+  return (
+    <div className='grid grid-cols-1 lg:grid-cols-5 gap-10'>
+      <div className='lg:col-span-1'>
+        <SectionLabel label='Billing Settings' msg='Add payment information, upgrade or modify your plan.'/>
+      </div>
+    </div>
+  )
+}
