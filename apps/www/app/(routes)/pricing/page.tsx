612e289c3d579b03a4ff597314f7d80c617a9f2e
Vinay <vinaymasina8740@gmail.com>
2024-11-09 18:58:55 +0530

    added pricing page

--- apps/www/app/(routes)/pricing/page.tsx
+++ apps/www/app/(routes)/pricing/page.tsx
@@ -1,10 +1,8 @@
+import PricingComponent from '@/components/custom/pricing/pricing'
 import React from 'react'
-import PricingComponent from '../../../components/custom/pricing/pricing'
 
 export default function page() {
   return (
-    <PricingComponent />
+    <PricingComponent/>
   )
 }
-
-
