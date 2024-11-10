6a49fa12784da55d98090c4fc4178dc60b35c9dd
Divyanshgupta030 <145568562+Divyanshgupta030@users.noreply.github.com>
2024-11-10 16:59:33 +0530

    error

--- apps/api/app/api/[[...route]]/route.ts
+++ apps/api/app/api/[[...route]]/route.ts
@@ -98,6 +98,6 @@ const GET = handle(app);
 const POST = handle(app);
 const PATCH = handle(app);
 const DELETE = handle(app);
-export const OPTIONS = handle(app);
+const OPTIONS = handle(app);
 
 export { GET, PATCH, POST, DELETE, OPTIONS };
