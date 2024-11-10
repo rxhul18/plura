38da0d5981fbb8f1e80af45a3d3cc29e7080eaf5
Saidev Dhal (Dev) <dev1974sai@gmail.com>
2024-11-10 19:40:41 +0530

    Revert "auth"

--- apps/www/app/(auth)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignInComponent from "@/components/custom/signinComponent";
-
-export default function page() {
-  return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <SignInComponent />
-    </div>
-  );
-}
