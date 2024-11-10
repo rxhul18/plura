00df11ba66d9152d6f6ac7a22d958e988089a084
Divyanshgupta030 <145568562+Divyanshgupta030@users.noreply.github.com>
2024-11-10 13:47:07 +0530

    fix

--- apps/www/components/custom/account-switcher.tsx
+++ apps/www/components/custom/account-switcher.tsx
@@ -1,46 +1,9 @@
 "use client";
-<<<<<<< HEAD
 import { authClient } from "../../lib/auth-client";
-=======
-import { authClient } from "@/lib/auth-client";
->>>>>>> 5b4aadf (add better-auth in hono)
 import { Session } from "@repo/auth";
 import { useRouter } from "next/navigation";
 interface Props {
   session: Session[];
-<<<<<<< HEAD
-  activeSession: Session;
-}
-export default function AccountSwitcher({ session, activeSession }: Props) {
-  const router = useRouter();
-  const onSelect = async (sessionId: string) => {
-    console.log(sessionId);
-    const active = await authClient.multiSession.setActive({
-      sessionId: sessionId,
-    });
-
-    console.log(active);
-    router.refresh();
-  };
-  return (
-    <div className="flex items-center justify-center gap-2 p-4">
-      <select
-        onChange={(e) => onSelect(e.target.value)}
-        value={activeSession.session.id}
-        className="border-2 border-gray-300[0.3] rounded-md p-2"
-      >
-        {session.map((item) => {
-          return (
-            <option key={item.session.id} value={item.session.id}>
-              {item.user.name}{" "}
-            </option>
-          );
-        })}
-      </select>
-    </div>
-  );
-}
-=======
   activeSession : Session;
 }
 export default function AccountSwitcher({session,activeSession}:Props) {
@@ -64,4 +27,3 @@ export default function AccountSwitcher({session,activeSession}:Props) {
     </div>
   )
 }
->>>>>>> 5b4aadf (add better-auth in hono)
