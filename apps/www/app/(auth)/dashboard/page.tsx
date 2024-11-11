4772af54ed084723d9157db602658ef2d9fdbc7b
Divyanshgupta030 <145568562+Divyanshgupta030@users.noreply.github.com>
2024-11-11 22:38:34 +0530

    fix error

--- apps/www/app/(auth)/dashboard/page.tsx
+++ apps/www/app/(auth)/dashboard/page.tsx
@@ -4,11 +4,11 @@ import { getMultipleSessions, getSession } from "@/lib/server";
 export default async function page() {
   const session = await getSession();
   const multipleSessions = await getMultipleSessions();
+  console.log(multipleSessions);
+  console.log(session);
   return (
     <div>
       <AccountSwitcher session={multipleSessions} activeSession={session} />
-      <pre className="font-sm">{JSON.stringify(session, null, 1)}</pre>
-      <pre>{JSON.stringify(multipleSessions, null, 2)}</pre>
     </div>
   );
 }
