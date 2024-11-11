11fce15ac7a2eda1c92717cb5bdc500807ee403a
Saidev Dhal <connect.saidev@gmail.com>
2024-11-11 19:38:11 +0530

    fix headers

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -26,10 +26,33 @@ const nextConfig = {
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
+          { key: "Access-Control-Expose-Headers", value: "Content-Length" },
+        ],
+      },
+      {
+        // Handle OPTIONS requests (pre-flight CORS)
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.plura.pro, https://app.plura.pro",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          // Handle pre-flight request for OPTIONS
+          { key: "Access-Control-Max-Age", value: "86400" }, // Cache pre-flight response for 24 hours
         ],
       },
     ];
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
