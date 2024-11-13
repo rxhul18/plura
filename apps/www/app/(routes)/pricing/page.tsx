4abf68a847393b221bdde72453daddc59fbfa416
Saidev Dhal <connect.saidev@gmail.com>
2024-11-13 18:14:56 +0530

    add auth page

--- apps/www/app/(routes)/pricing/page.tsx
+++ apps/www/app/(routes)/pricing/page.tsx
@@ -7,17 +7,18 @@ import PricingCards from "@/components/custom/pricing/pricing.cards";
 
 export default function Pricing() {
   return (
-    <div className="container relative">
+    <div className="flex flex-col items-center justify-center">
       <PageHeader className="gap-2">
         <h2 className="max-w-5xl mx-auto text-center tracking-tight font-medium text-white text-3xl md:text-4xl md:leading-tight">
-        Get instant access to all components and templates
+          Get instant access to all components and templates
         </h2>
         <PageHeaderDescription className="max-w-3xl mx-auto tracking-tight ">
-        For a one-time payment, you get access to all components and templates, including future updates and new templates.
+          For a one-time payment, you get access to all components and
+          templates, including future updates and new templates.
         </PageHeaderDescription>
       </PageHeader>
-      <section id="pricing">
-        <PricingCards/>
+      <section id="pricing" className="flex items-center justify-center">
+        <PricingCards />
       </section>
     </div>
   );
--- apps/www/components/custom/site/footer.tsx
+++ apps/www/components/custom/site/footer.tsx
@@ -1,7 +1,7 @@
 export function SiteFooter() {
   return (
     <footer className="py-6 md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+      <div className="flex flex-col items-center gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
           &copy; 2024 Plura Ai. All rights reserved.
         </p>
