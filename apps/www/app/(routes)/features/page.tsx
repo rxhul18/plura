195beefa402257f5a9abe83fec95a0977a3085b1
Ashutosh Dash <dashashutosh1999@gmail.com>
2024-12-03 12:53:15 +0530

    feat: feature page ui

--- apps/www/app/(routes)/features/page.tsx
+++ apps/www/app/(routes)/features/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
+
+import FeatureCard from "@/components/custom/features/feature.card";
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/custom/text-wrappers";
+import BlurFade from "@/components/ui/blur-fade";
 
 export default function Features() {
   return (
-    <div>
-      Fuck
+    <div className="flex flex-col items-center justify-center pb-8">
+      <div className="absolute inset-0 mx-auto h-full w-full bg-[radial-gradient(circle,rgba(211,211,211,0.1),rgba(18,20,22,0.05),rgba(18,20,22,0))] opacity-60" />
+      <PageHeader className="flex items-center justify-center md:items-center">
+        <BlurFade delay={0.2} inView>
+          <PageHeaderHeading>Features</PageHeaderHeading>
+        </BlurFade>
+
+        <BlurFade delay={0.2 * 2} inView>
+          <PageHeaderDescription className="text-center">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt
+            saepe earum delectus quas fuga eligendi voluptatem doloremque neque
+            esse.
+          </PageHeaderDescription>
+        </BlurFade>
+      </PageHeader>
+
+      <BlurFade
+        delay={0.2 * 3}
+        inView
+        className="w-full grid grid-cols-2 grid-rows-3  gap-6"
+      >
+        <FeatureCard className="col-span-2" />
+        <FeatureCard className="row-span-2" />
+        <FeatureCard />
+        <FeatureCard />
+        <FeatureCard />
+        <FeatureCard />
+        <FeatureCard className="col-span-2" />
+      </BlurFade>
     </div>
-  )
+  );
 }
---
+++ apps/www/components/custom/features/feature.card.tsx
@@ -0,0 +1,51 @@
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  SectionHeaderDescription,
+  SectionHeaderHeading,
+} from "../text-wrappers";
+
+const FeatureCard = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <Card
+      className={cn(
+        "bg-card rounded-3xl p-4 hover:bg-muted/50 transition-all ease-in-out duration-300 w-full group flex flex-col justify-between gap-3",
+        className
+      )}
+      {...props}
+    >
+      <Image
+        src="/images/usagehome.jpg"
+        alt="image"
+        height={300}
+        width={300}
+        draggable={false}
+        className="w-full rounded-xl object-cover flex-1 min-h-60"
+      />
+      <div className="flex justify-between flex-wrap items-center gap-3">
+        <div>
+          <SectionHeaderDescription className="">
+            Features
+          </SectionHeaderDescription>
+          <SectionHeaderHeading className="text-3xl md:text-4xl break-all">
+            Lorem ipsum dolor sit amet.
+          </SectionHeaderHeading>
+        </div>
+
+        <Link
+          href={""}
+          className="border rounded-full p-3 group-hover:bg-neutral-600 transition-all ease-in-out duration-300"
+        >
+          <ChevronRight size={16} />
+        </Link>
+      </div>
+    </Card>
+  );
+};
+export default FeatureCard;
