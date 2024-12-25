a57b7eaaea575eec216132b24dfba20e44f7579c
Saidev Dhal <connect.saidev@gmail.com>
2024-12-25 23:57:49 +0530

    core: update discord bot commands

--- apps/services/discord/src/events/bot/alive.ts
+++ apps/services/discord/src/events/bot/alive.ts
@@ -1,14 +1,16 @@
 import { Client } from "discord.js";
 
 export default async function alive(client: Client) {
-    client.on("ready", async () => {
-        console.log(`${client.user?.username} is ready! 🤖`)
-        client.user?.setPresence({
-            activities: [{
-                name: `${client.config?.prefix}help | plura.pro`,
-                type: 3,
-            }],
-            status: "idle"
-        });
+  client.on("ready", async () => {
+    console.log(`${client.user?.username} is ready! 🤖`);
+    client.user?.setPresence({
+      activities: [
+        {
+          name: `${client.config?.prefix}help | plura.pro`,
+          type: 3,
+        },
+      ],
+      status: "idle",
     });
+  });
 }
