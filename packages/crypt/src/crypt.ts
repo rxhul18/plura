f51382463998a036f8132c5f632a8250cfc03620
ayushhunt <ayushoo7hunt@gmail.com>
2024-12-27 22:58:57 +0530

    Updated the crypt package to use web crypto api  and secured post route of workspace which stores the name of the user to db

--- packages/crypt/src/crypt.ts
+++ packages/crypt/src/crypt.ts
@@ -1,35 +1,49 @@
-import * as crypto from "crypto";
-
-import { Buffer } from "buffer";
-
 interface EncryptedData {
-  initialVector: string;
-  encrypted: string;
-  authTag: string;
+  initialVector:string,
+  encrypted:string,
 }
 
-export function encrypt(text: string, key: Buffer): EncryptedData {
-  if (key.length !== 32) {
+function encode(text:string):Uint8Array {
+  return new TextEncoder().encode(text);
+}
+
+
+function decode(buffer:Uint8Array): string {
+  return new TextDecoder().decode(buffer);
+}
+
+
+export async function encrypt(text:string, key:Uint8Array):Promise<EncryptedData> {
+  if(key.length !== 32){
     throw new Error("Key must be 32 bytes long for AES-256-GCM encryption");
   }
 
   try {
-    const initialVector = crypto.randomBytes(12);
-    const cipher = crypto.createCipheriv(
-      "aes-256-gcm",
-      Buffer.from(key),
-      initialVector,
+    const iv = crypto.getRandomValues(new Uint8Array(12));
+
+    const cryptoKey = await crypto.subtle.importKey(
+      "raw",
+      key,
+      {name:"AES-GCM"},
+      false,
+      ["encrypt"]
     );
-    let encrypted = cipher.update(text, "utf8", "hex");
-    encrypted += cipher.final("hex");
-    const authTag = cipher.getAuthTag();
+
+    const encryptedData = await crypto.subtle.encrypt(
+      {
+        name:"AES-GCM",
+        iv:iv, 
+      },
+      cryptoKey,
+      encode(text)
+    )
+
     return {
-      initialVector: initialVector.toString("hex"),
-      encrypted: encrypted,
-      authTag: authTag.toString("hex"),
-    };
+      initialVector: Buffer.from(iv).toString("hex"),
+      encrypted: Buffer.from(encryptedData).toString("hex"),
+    }
   } catch (error: unknown) {
-    if (error instanceof Error) {
+    if(error instanceof Error){
       throw new Error("Encryption failed: " + error.message);
     } else {
       throw new Error("Encryption failed: unknown error");
@@ -37,26 +51,39 @@ export function encrypt(text: string, key: Buffer): EncryptedData {
   }
 }
 
-export function decrypt(encryptedData: EncryptedData, key: Buffer): string {
-  if (key.length !== 32) {
+export async function decrypt(encryptedData:EncryptedData,key:Uint8Array):Promise<string> {
+  if(key.length !== 32) {
     throw new Error("Key must be 32 bytes long for AES-256-GCM decryption");
   }
 
   try {
-    const initialVector = Buffer.from(encryptedData.initialVector, "hex");
-    const encrypted = encryptedData.encrypted;
-    const authTag = Buffer.from(encryptedData.authTag, "hex");
-    const decipher = crypto.createDecipheriv("aes-256-gcm", key, initialVector);
-    decipher.setAuthTag(authTag);
-    let decrypted = decipher.update(encrypted, "hex", "utf8");
-    decrypted += decipher.final("utf8");
+    const iv = Buffer.from(encryptedData.initialVector,"hex");
+    const encryptedBuffer = Buffer.from(encryptedData.encrypted,"hex");
 
-    return decrypted;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      throw new Error("Decryption failed: " + error.message);
+    const cyptoKey = await crypto.subtle.importKey(
+      "raw",
+      key,
+      {name:"AES-GCM"},
+      false,
+      ["decrypt"]
+    )
+
+    const decryptedData = await crypto.subtle.decrypt(
+      {
+        name:"AES-GCM",
+        iv:iv,
+      },
+      cyptoKey,
+      encryptedBuffer
+    )
+
+    return decode(new Uint8Array(decryptedData));
+  }
+  catch (error: unknown){
+    if(error instanceof Error){
+      throw new Error("Encryption failed: " + error.message);
     } else {
-      throw new Error("Decryption failed: unknown error");
+      throw new Error("Encryption failed: unknown error");
     }
   }
 }
