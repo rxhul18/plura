fc35dbb51ea86c02262087b3c97f497597edb0b4
Navodit <navoditbharat@gmail.com>
2025-01-01 23:01:06 +0530

    used command in place of dialog

--- apps/app/components/custom/sidebar/QuickActionButton.tsx
+++ apps/app/components/custom/sidebar/QuickActionButton.tsx
@@ -14,6 +14,14 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Badge } from "@/components/ui/badge";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 
 const QuickActionButton = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -35,36 +43,24 @@ const QuickActionButton = () => {
 
   return (
     <div>
-      <Dialog open={isOpen} onOpenChange={setIsOpen}>
-        <DialogTrigger asChild>
-          <Button className="w-full justify-between flex items-center font-semibold border text-center gap-x-1 text-sm leading-5 rounded-md py-1.5 h-8 px-2 shadow-10 hover:shadow-lg">
-            <span className="flex items-center gap-x-1">
-              <Zap />
-              Quick Actions
-            </span>
+      <Button variant="outline">
+        Quick Actions
+        <Badge className="block px-2 py-0.5 rounded-md text-xs font-semibold hover:bg-none">
+          Ctrl + K{" "}
+        </Badge>
+      </Button>
 
-            <Badge
-              variant="secondary"
-              className="block px-2 py-0.5 rounded-md text-xs font-semibold hover:bg-secondary"
-            >
-              Ctrl + K{" "}
-            </Badge>
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-md">
-          <DialogHeader>
-            <DialogTitle>Sample Dialog Title</DialogTitle>
-            <DialogDescription>Sample Dialog</DialogDescription>
-          </DialogHeader>
-          <DialogFooter className="sm:justify-start">
-            <DialogClose asChild>
-              <Button type="button" variant="secondary">
-                Close
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+      <CommandDialog open={isOpen} onOpenChange={setIsOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
     </div>
   );
 };
